aaf040a4333828b1221caa9310a8adf37c08fb5e
Haifa salim <123863827+HaifaSalim@users.noreply.github.com>
2025-09-23 09:31:41 +0400

    Update WebConfig.java

--- src/main/java/com/chatbot/WebConfig.java
+++ src/main/java/com/chatbot/WebConfig.java
@@ -1,20 +1,14 @@
 package com.chatbot;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-	    return new WebMvcConfigurer() {
-	        @Override
-	        public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/**").allowedOrigins("https://gemini-based-chatbot-eqczbbf3dthkg2fr.uaenorth-01.azurewebsites.net"); 
-	        }
-	    };
-	}}
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
+	}
+}
